cd03c18cc88447e8ef4e16168a76365548fde873
Sebastian Hans Weissenborn <58604870+s2seweis@users.noreply.github.com>
2023-11-30 14:09:29 +0100

    disbaled email recaptcha

--- src/pages/Contact/ContactForm.js
+++ src/pages/Contact/ContactForm.js
@@ -4,21 +4,22 @@ export default function ContactForm() {
   return (
     <div className="contact__card">
       <form action="https://formsubmit.co/weissenborn.sebastian@gmail.com" method="POST">
-        <input type='hidden' name='subject' value="New email, OMG!"/>
-        <input type="hidden" name="_next" value="https://idyllic-mooncake-7b63ff.netlify.app/thanks.html"/>
-          <div className="form-group">
-            <label htmlFor="name">Your Name</label>
-            <input type="text" id="name" name="name" required />
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">Your Email</label>
-            <input type="email" id="email" name="email" required />
-          </div>
-          <div className="form-group">
-            <label htmlFor="message">Your Message</label>
-            <textarea id="message" name="message" rows="4" required></textarea>
-          </div>
-          <button className='button-contact' type="submit">Send Message</button>
+        <input type='hidden' name='subject' value="New email, OMG!" />
+        <input type="hidden" name="_captcha" value="false"/>
+        <input type="hidden" name="_next" value="https://idyllic-mooncake-7b63ff.netlify.app/thanks.html" />
+        <div className="form-group">
+          <label htmlFor="name">Your Name</label>
+          <input type="text" id="name" name="name" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Your Message</label>
+          <textarea id="message" name="message" rows="4" required></textarea>
+        </div>
+        <button className='button-contact' type="submit">Send Message</button>
       </form>
     </div>
   )
