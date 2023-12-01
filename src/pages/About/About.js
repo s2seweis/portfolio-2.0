b49744dd6279842bff3a83a7541057a1d1145687
Sebastian Hans Weissenborn <58604870+s2seweis@users.noreply.github.com>
2023-12-01 10:26:09 +0100

    upload

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import React from 'react'
 import './About.css'
-import lebenslauf from '../../assets/cv.pdf'
+import cv from '../../assets/cv1.pdf'
 // import Lottie from 'lottie-react'
 // import data from '../../images/data.json'
 
@@ -25,7 +25,7 @@ function About({ scrollValue }) {
               <h1 className="about__greetings">Hi, I'm <span className="about__name">Sebastian Weissenborn</span> </h1>
               <p className="about__text">Looking for a job opportunity for a ReactJS Developer position to start my career in Web Development. My passion is focused on developing applications with the use of Modern Javascript Library and Frameworks.</p>
             </div>
-            <a style={{margin:"20px auto", padding:"15px", width:"auto", fontSize:"15px"}} href={lebenslauf} download className='download__cv'>Download CV</a>
+            <a style={{margin:"20px auto", padding:"15px", width:"auto", fontSize:"15px"}} href={cv} download className='download__cv'>Download CV</a>
           </div>      
         </div>
       </div>
